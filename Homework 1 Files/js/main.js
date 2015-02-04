7b26d7a52e6387117976fbfe6242b7612ae3070d
David Gall <dlaker74@fullsail.edu>
2015-02-04 14:39:49 -0800

    Bezier
    Finished the star by drawing a star in adobe illustrator to get
    coordinates. Working on my bezier curve…slowly coming along.

--- Homework 1 Files/js/main.js
+++ Homework 1 Files/js/main.js
@@ -35,53 +35,54 @@ Reminder - set the style first then draw.
 ********************************************/
 
 //Draw Rectangle here
-window.onload = function(){
+//window.onload = function(){
+
+if(Modernizr.canvas) {
 
     var myCanvas = document.getElementById("Canvas1");
-    if(myCanvas && myCanvas.getContext){
+    if (myCanvas && myCanvas.getContext) {
         var context = myCanvas.getContext("2d");
 
-        if(context){
+        if (context) {
 
             context.fillStyle = "blue";
             context.strokeStyle = "black";
             context.lineWidth = 4;
-            context.fillRect(0,0,50,100);
-            context.strokeRect(0,0,50,100);
+            context.fillRect(0, 0, 50, 100);
+            context.strokeRect(0, 0, 50, 100);
             context.stroke();
 
         }
     }
 
 
+    /*******************************************
+     PART 2
 
-/*******************************************
-PART 2
+     Draw a circle starting at point (50 ,50)
+     That has a radius of 20 px
+     Set the color of the circle to a shade of red and set the alpha to .5
+     Set the stroke color to black and use a radius of 30px for this circle.
 
-Draw a circle starting at point (50 ,50)
-That has a radius of 20 px 
-Set the color of the circle to a shade of red and set the alpha to .5
-Set the stroke color to black and use a radius of 30px for this circle.
-
-Reminder - set the style first then draw.
-Use the arc method
-********************************************/
+     Reminder - set the style first then draw.
+     Use the arc method
+     ********************************************/
 
 
 //Draw Circle here
 
 
     var myCanvas2 = document.getElementById("Canvas2");
-    if(myCanvas2 && myCanvas2.getContext){
+    if (myCanvas2 && myCanvas2.getContext) {
         var context2 = myCanvas2.getContext("2d");
-        if(context2){
+        if (context2) {
 
             context2.globalAlpha = .5;
             context2.fillStyle = "red";
             context2.strokeStyle = "black";
 
             context2.beginPath();
-            context2.arc(50,50,20, 0,2*Math.PI);
+            context2.arc(50, 50, 20, 0, 2 * Math.PI);
             context2.fill();
 
             context2.stroke();
@@ -91,99 +92,126 @@ Use the arc method
     }
 
 
+    /*******************************************
+     PART 3
 
+     Practice using Path drawing.
+     Create a 5-point star shaped pattern using the lineTo method.
+     Begin this shape at (100, 100)
 
+     Height and width and color are up to you.
 
-
-
-
-/*******************************************
-PART 3
-
-Practice using Path drawing.
-Create a 5-point star shaped pattern using the lineTo method.
-Begin this shape at (100, 100)
-
-Height and width and color are up to you.
-
-********************************************/
+     ********************************************/
 
 
 //Draw Star here
 
     var myCanvas3 = document.getElementById("Canvas3");
-    if(myCanvas3 && myCanvas3.getContext){
+    if (myCanvas3 && myCanvas3.getContext) {
         var context3 = myCanvas3.getContext("2d");
-        if(context3){
+        if (context3) {
 
-            context3.strokeStyle = "black";
+            context3.strokeStyle = "green";
             context3.lineWidth = 4;
+            context3.fillStyle = "blue";
             context3.beginPath();
-            context3.moveTo(100,100);
-            context3.lineTo(140,20);
-            context3.lineTo(190, 100);
-            context3.lineTo(280,80);
-            context3.lineTo(230,170);
-            context3.lineTo(270,190);
+            context3.moveTo(100, 100);
+            context3.lineTo(113,76);
+            context3.lineTo(125,100);
+            context3.lineTo(152,105);
+            context3.lineTo(132,124);
+            context3.lineTo(137,150);
+            context3.lineTo(113,138);
+            context3.lineTo(89,150);
+            context3.lineTo(93,124);
+            context3.lineTo(74,104);
+            context3.closePath();
+            context3.fill();
             context3.stroke();
+//Created a star in Adobe Illustrator and tracked the points.
+
+        }
+    }
+
+    /*******************************************
+     PART 4
+
+     Practice drawing with Bezier curves.
+     Try drawing the top to an umbrella.
+     This should have one large arc (a half circle) on the top and scalloped edges on the bottom.
+
+     Position, height, width and color are your choice.
+     Do not overlap any other object.
+
+     ********************************************/
+
+//Draw Umbrella top here
+
+    var myCanvas4 = document.getElementById("Canvas4");
+    if(myCanvas4 && myCanvas4.getContext){
+        var context4 = myCanvas4.getContext("2d");
+
+        if(context4){
+
+            context4.strokeStyle = "black";
+            context4.lineWidth = 1;
+            context4.fillStyle = "red";
+
+            context4.beginPath();
+            context4.arc(75,75,50,Math.PI,2*Math.PI);
+            //context4.moveTo(75,75);
+            //context4.bezierCurveTo(65,65, 65,75,200,75);
+            context4.bezierCurveTo(75,30,60,40,75,75);
+            //context4.lineTo(25,75);
+            //context4.closePath();
+
+            context4.stroke();
+
+
 
 
         }
     }
 
-/*******************************************
-PART 4
+    /*******************************************
+     PART 5
 
-Practice drawing with Bezier curves.
-Try drawing the top to an umbrella.
-This should have one large arc (a half circle) on the top and scalloped edges on the bottom.
+     Practice using text.
+     Draw text into your canvas.  It can said whatever you would like in any color.
 
-Position, height, width and color are your choice.
-Do not overlap any other object.
-
-********************************************/
-
-//Draw Umbrella top here
-
-/*******************************************
-PART 5
-
-Practice using text.
-Draw text into your canvas.  It can said whatever you would like in any color.
-
-********************************************/
+     ********************************************/
 
 //Draw text here
 
-/*******************************************
-PART 6
+    /*******************************************
+     PART 6
 
-Pixel manipulation.
-Draw the image logo.png into the canvas in the following 3 ways.
-1. The image exactly as it is.
-2. Shrink the image by 50%
-3. Slice a section of the logo out and draw that onto the canvas.
+     Pixel manipulation.
+     Draw the image logo.png into the canvas in the following 3 ways.
+     1. The image exactly as it is.
+     2. Shrink the image by 50%
+     3. Slice a section of the logo out and draw that onto the canvas.
 
-Reminder to use the drawImage method for all 3 of the ways.
+     Reminder to use the drawImage method for all 3 of the ways.
 
-********************************************/
+     ********************************************/
 
 //Draw images here
 
 
+    /*******************************************
+     PART 7
 
-/*******************************************
-PART 7
+     Putting it all together.
 
-Putting it all together. 
+     Using a combination of all the methods.
+     Create a complex scene.
+     You must use at least 3 different methods.
 
-Using a combination of all the methods. 
-Create a complex scene.
-You must use at least 3 different methods.
-
-********************************************/
+     ********************************************/
 
 // Draw scene here
 
 
-};
+//};
+}
